439e6c7276486e00e35242ab64adb45c702995c2
Joshua Brown <josh.bikedude.419@gmail.com>
2023-12-04 16:40:02 -0800

    optimizations?

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,5 +1,5 @@
 use crate::{Solution, SolutionPair};
-use std::{fs::read_to_string, ops::Index};
+use std::{fs::read_to_string};
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -8,69 +8,69 @@ const digits : [&str; 10] = ["zero", "one", "two", "three", "four", "five", "six
 pub fn solve() -> SolutionPair {
     let input = read_to_string("input/day1.txt").expect("");
     // Your solution here...
-    let mut sol1: u64 = 0;
+    let mut sol1: usize = 0;
 
-    let mut f1 : char = '0';
-    let mut l1 : char = '0';
+    let mut f1 : usize = 0;
+    let mut l1 : usize = 0;
     for line in input.split("\n") {
         for c in line.chars() {
             if c.is_ascii_digit() {
-                f1 = c;
+                f1 = c as usize - 0x30;
                 break;
             }
         }
 
         for c in line.chars().rev() {
             if c.is_ascii_digit() {
-                l1 = c;
+                l1 = c as usize - 0x30;
                 break;
             }
         }
 
-        sol1 += format!("{}{}", f1, l1).parse::<u64>().unwrap()
+        sol1 += f1 * 10 + l1
     }
 
-    let mut sol2: u64 = 0;
-    let mut f2 : char = '0';
-    let mut l2 : char = '0';
+    let mut sol2: usize = 0;
+    let mut f2 : usize = 0;
+    let mut l2 : usize = 0;
     for line in input.split("\n") {
         let chars : Vec<char> = line.chars().collect();
         for i in 0..line.len() {
             if chars[i].is_ascii_digit() {
-                f2 = chars[i];
+                f2 = chars[i] as usize - 0x30;
                 break;
             }
 
             if i > 1 && digits.contains(&String::from_iter(&chars[i-2..i+1]).as_str()){
-                f2 = char::from_digit(digits.iter().position(|&r| r == String::from_iter(&chars[i-2..i+1]).as_str()).unwrap() as u32, 10).unwrap();
+                f2 = digits.iter().position(|&r| r == String::from_iter(&chars[i-2..i+1]).as_str()).unwrap();
                 break;
             } else if i > 2 && digits.contains(&String::from_iter(&chars[i-3..i+1]).as_str()) {
-                f2 = char::from_digit(digits.iter().position(|&r| r == String::from_iter(&chars[i-3..i+1]).as_str()).unwrap() as u32, 10).unwrap();
+                f2 = digits.iter().position(|&r| r == String::from_iter(&chars[i-3..i+1]).as_str()).unwrap();
                 break;
             } else if i > 3 && digits.contains(&String::from_iter(&chars[i-4..i+1]).as_str()) {
-                f2 = char::from_digit(digits.iter().position(|&r| r == String::from_iter(&chars[i-4..i+1]).as_str()).unwrap() as u32, 10).unwrap();
+                f2 = digits.iter().position(|&r| r == String::from_iter(&chars[i-4..i+1]).as_str()).unwrap();
                 break;
             }
         }
 
         for j in (0..line.len()).rev() {
             if chars[j].is_ascii_digit() {
-                l2 = chars[j];
+                l2 = chars[j] as usize - 0x30;
                 break;
             }
 
             if j > 1 && digits.contains(&String::from_iter(&chars[j-2..j+1]).as_str()){
-                l2 = char::from_digit(digits.iter().position(|&r| r == String::from_iter(&chars[j-2..j+1]).as_str()).unwrap() as u32, 10).unwrap();
+                l2 = digits.iter().position(|&r| r == String::from_iter(&chars[j-2..j+1]).as_str()).unwrap();
                 break;
             } else if j > 2 && digits.contains(&String::from_iter(&chars[j-3..j+1]).as_str()) {
-                l2 = char::from_digit(digits.iter().position(|&r| r == String::from_iter(&chars[j-3..j+1]).as_str()).unwrap() as u32, 10).unwrap();
+                l2 = digits.iter().position(|&r| r == String::from_iter(&chars[j-3..j+1]).as_str()).unwrap();
                 break;
             } else if j > 3 && digits.contains(&String::from_iter(&chars[j-4..j+1]).as_str()) {
-                l2 = char::from_digit(digits.iter().position(|&r| r == String::from_iter(&chars[j-4..j+1]).as_str()).unwrap() as u32, 10).unwrap();
+                l2 = digits.iter().position(|&r| r == String::from_iter(&chars[j-4..j+1]).as_str()).unwrap();
                 break;
             }
         }
-        sol2 += format!("{}{}", f2, l2).parse::<u64>().unwrap()
+        sol2 += f2 * 10 + l2
     }
 
     (Solution::from(sol1), Solution::from(sol2))
