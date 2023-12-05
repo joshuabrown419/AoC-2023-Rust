4b196d85767fac67626b61c1a0582045a7c6598e
Joshua Brown <josh.bikedude.419@gmail.com>
2023-12-04 16:59:30 -0800

    maybe more effecient

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -31,47 +31,70 @@ pub fn solve() -> SolutionPair {
     }
 
     let mut sol2: usize = 0;
-//    let mut f2 : usize = 0;
-//    let mut l2 : usize = 0;
-//    for line in input.split("\n") {
-//        let chars : Vec<char> = line.chars().collect();
-//        for i in 0..line.len() {
-//            if chars[i].is_ascii_digit() {
-//                f2 = chars[i] as usize - 0x30;
-//                break;
-//            }
-//
-//            if i > 1 && DIGITS.contains(&String::from_iter(&chars[i-2..i+1]).as_str()){
-//                f2 = DIGITS.iter().position(|&r| r == String::from_iter(&chars[i-2..i+1]).as_str()).unwrap();
-//                break;
-//            } else if i > 2 && DIGITS.contains(&String::from_iter(&chars[i-3..i+1]).as_str()) {
-//                f2 = DIGITS.iter().position(|&r| r == String::from_iter(&chars[i-3..i+1]).as_str()).unwrap();
-//                break;
-//            } else if i > 3 && DIGITS.contains(&String::from_iter(&chars[i-4..i+1]).as_str()) {
-//                f2 = DIGITS.iter().position(|&r| r == String::from_iter(&chars[i-4..i+1]).as_str()).unwrap();
-//                break;
-//            }
-//        }
-//
-//        for j in (0..line.len()).rev() {
-//            if chars[j].is_ascii_digit() {
-//                l2 = chars[j] as usize - 0x30;
-//                break;
-//            }
-//
-//            if j > 1 && DIGITS.contains(&String::from_iter(&chars[j-2..j+1]).as_str()){
-//                l2 = DIGITS.iter().position(|&r| r == String::from_iter(&chars[j-2..j+1]).as_str()).unwrap();
-//                break;
-//            } else if j > 2 && DIGITS.contains(&String::from_iter(&chars[j-3..j+1]).as_str()) {
-//                l2 = DIGITS.iter().position(|&r| r == String::from_iter(&chars[j-3..j+1]).as_str()).unwrap();
-//                break;
-//            } else if j > 3 && DIGITS.contains(&String::from_iter(&chars[j-4..j+1]).as_str()) {
-//                l2 = DIGITS.iter().position(|&r| r == String::from_iter(&chars[j-4..j+1]).as_str()).unwrap();
-//                break;
-//            }
-//        }
-//        sol2 += f2 * 10 + l2
-//    }
+    let mut f2 : usize = 0;
+    let mut l2 : usize = 0;
+    for line in input.split("\n") {
+        let chars : Vec<char> = line.chars().collect();
+        for i in 0..line.len() {
+            if chars[i].is_ascii_digit() {
+                f2 = chars[i] as usize - 0x30;
+                break;
+            }
+
+            if i > 1 && DIGITS.contains(&String::from_iter(&chars[i-2..i+1]).as_str()){
+                f2 = slice_to_usize(&chars[i-2..i+1]);
+                break;
+            } else if i > 2 && DIGITS.contains(&String::from_iter(&chars[i-3..i+1]).as_str()) {
+                f2 = slice_to_usize(&chars[i-3..i+1]);
+                break;
+            } else if i > 3 && DIGITS.contains(&String::from_iter(&chars[i-4..i+1]).as_str()) {
+                f2 = slice_to_usize(&chars[i-4..i+1]);
+                break;
+            }
+        }
+
+        for j in (0..line.len()).rev() {
+            if chars[j].is_ascii_digit() {
+                l2 = chars[j] as usize - 0x30;
+                break;
+            }
+
+            if j > 1 && DIGITS.contains(&String::from_iter(&chars[j-2..j+1]).as_str()){
+                l2 = slice_to_usize(&chars[j-2..j+1]);
+                break;
+            } else if j > 2 && DIGITS.contains(&String::from_iter(&chars[j-3..j+1]).as_str()) {
+                l2 = slice_to_usize(&chars[j-3..j+1]);
+                break;
+            } else if j > 3 && DIGITS.contains(&String::from_iter(&chars[j-4..j+1]).as_str()) {
+                l2 = slice_to_usize(&chars[j-4..j+1]);
+                break;
+            }
+        }
+        sol2 += f2 * 10 + l2
+    }
 
     (Solution::from(sol1), Solution::from(sol2))
+}
+
+pub fn slice_to_usize(input: &[char]) -> usize {
+    if input == ['o', 'n', 'e'] {
+        return 1;
+    } else if input == ['t', 'w', 'o'] {
+        return 2;
+    } else if input == ['t', 'h', 'r', 'e', 'e'] {
+        return 3;
+    } else if input == ['f', 'o', 'u', 'r'] {
+        return 4;
+    } else if input == ['f', 'i', 'v', 'e'] {
+        return 5;
+    } else if input == ['s', 'i', 'x'] {
+        return 6;
+    } else if input == ['s', 'e', 'v', 'e', 'n'] {
+        return 7;
+    } else if input == ['e', 'i', 'g', 'h', 't'] {
+        return 8;
+    } else if input == ['n', 'i', 'n', 'e'] {
+        return 9;
+    }
+    return 0;
 }
